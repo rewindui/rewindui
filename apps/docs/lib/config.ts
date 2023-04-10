358fba5e2965e9b2e31cc3369110d7a1a112394d
Nick Dunas <nntounas@gmail.com>
2023-04-10 12:46:29 +0300

    fix: corrected github and storybook links

--- apps/docs/lib/config.ts
+++ apps/docs/lib/config.ts
@@ -1,9 +1,9 @@
 export const config = {
   GITHUB_REPO_COMPONENTS:
-    'https://github.com/rewindui/rewindui/tree/main/packages/rewindui-core/src/components',
+    'https://github.com/rewindui/rewindui/tree/main/packages/core/src/components',
   GITHUB_REPO_THEME:
-    'https://github.com/rewindui/rewindui/tree/main/packages/rewindui-core/src/theme/styles',
+    'https://github.com/rewindui/rewindui/tree/main/packages/core/src/theme/styles',
   GITHUB_REPO_VARIANTS:
-    'https://github.com/rewindui/rewindui/tree/main/packages/rewindui-core/src/theme/variants',
-  STORYBOOK_URL: 'https://rewindui-docs.vercel.app/?path=/story/components-',
+    'https://github.com/rewindui/rewindui/tree/main/packages/core/src/theme/variants',
+  STORYBOOK_URL: 'https://storybook.rewind-ui.dev/?path=/story/components-',
 };
