59599be5f979d7233a42e88d6463730778938f70
Nick Dunas <nntounas@gmail.com>
2023-04-11 16:10:59 +0300

    docs: hotfix

--- apps/docs/ui/Aside.tsx
+++ apps/docs/ui/Aside.tsx
@@ -3,7 +3,7 @@ import { Menu } from './Menu';
 
 export const Aside = () => {
   return (
-    <aside className="fixed py-8 h-[calc(100vh-10rem)] w-full overflow-auto">
+    <aside className="fixed py-8 h-[calc(100vh-10rem)] w-[16rem] overflow-auto">
       <Menu />
     </aside>
   );
