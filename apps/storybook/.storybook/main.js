8a10e007aa6c69e15182cac21ccb59ceb7def789
Nick Dunas <nntounas@gmail.com>
2023-07-17 21:31:58 +0300

    chore: upgrade to storybook 7

--- apps/storybook/.storybook/main.js
+++ apps/storybook/.storybook/main.js
@@ -1,18 +1,22 @@
 const path = require('path');
-
 module.exports = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.tsx'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-vite',
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
   },
   async viteFinal(config, { configType }) {
     // customize the Vite config here
     return {
       ...config,
       esbuild: {
-        logOverride: { 'this-is-undefined-in-esm': 'silent' },
+        logOverride: {
+          'this-is-undefined-in-esm': 'silent',
+        },
+      },
+      define: {
+        'process.env': {},
       },
       resolve: {
         alias: [
@@ -23,5 +27,8 @@ module.exports = {
         ],
       },
     };
+  },
+  docs: {
+    autodocs: true,
   },
 };
