234a476d8eaf3d6c33b502a3c0de2fd6fe3a23ba
Nick Dunas <nntounas@gmail.com>
2023-07-19 15:29:53 +0300

    feat: moved from happy-dom to jsdom

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -1,10 +1,12 @@
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [tsconfigPaths(), react()],
   // @ts-ignore
   test: {
-    environment: 'happy-dom',
+    globals: true,
+    environment: 'jsdom',
   },
 });
