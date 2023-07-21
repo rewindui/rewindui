ad0cf44268f4e4f8e46928a2232732943e64dbf1
Nick Dunas <nntounas@gmail.com>
2023-07-21 12:38:13 +0300

    docs: updated scrollbar

--- apps/docs/ui/Aside.tsx
+++ apps/docs/ui/Aside.tsx
@@ -3,7 +3,7 @@ import { Menu } from './Menu';
 
 export const Aside = () => {
   return (
-    <aside className="fixed py-8 h-[calc(100%-9rem)] w-[16rem] overflow-auto">
+    <aside className="fixed py-8 h-[calc(100%-9rem)] w-[16rem] scrollbar scrollbar-thin scrollbar-rounded-lg scrollbar-thumb-gray-200 scrollbar-track-gray-100 overflow-auto">
       <Menu />
     </aside>
   );
