bd31e4f348662ef660849f1db347405b586c1596
Nick Dunas <nntounas@gmail.com>
2023-09-26 14:44:06 +0300

    dashboard: metadata

--- apps/dashboard/app/layout.tsx
+++ apps/dashboard/app/layout.tsx
@@ -1,12 +1,45 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { config } from '@/lib/config';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://dashboard.rewind-ui.dev'),
   title: 'Rewind-UI - Admin Dashboard',
   description: 'Effortless interfaces',
+  generator: 'Next.js',
+  applicationName: 'Rewind-UI - Admin Dashboard',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Next.js', 'React', 'Tailwind CSS', 'UI', 'React component library'],
+  authors: [{ name: 'Nick Dunas' }],
+  colorScheme: 'light',
+  creator: 'Nick Dunas',
+  publisher: 'Nick Dunas',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  openGraph: {
+    title: {
+      default: config.metadata.title,
+      template: 'Rewind-UI - %s',
+    },
+  },
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: config.metadata.title,
+    description: config.metadata.description,
+    images: [config.metadata.ogImage],
+    creator: '@NickDunas',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
---
+++ apps/dashboard/lib/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+  metadata: {
+    title: 'Rewind-UI',
+    description: 'Effortless user interfaces',
+    ogImage: '/images/og-image.jpg',
+  },
+};
